c09f0e91ec8d7e11f69c5e63e6c136c99e7533e3
tiansy97 <59889947+tiansy97@users.noreply.github.com>
2021-09-29 00:02:07 +0800

    Update code style

--- project 1/motor/motor.ino
+++ project 1/motor/motor.ino
@@ -4,108 +4,106 @@ int Pin3 = 7;
 int Pin4 = 8;    
 
 int StepCount = 0;
-boolean dir = true;      //Direction of motion 
+boolean dir = true;  //Direction of motion 
 int stepN = 0;
 int stepmax = 800*15;
 
 
 void setup()
 {
-  pinMode(Pin1, OUTPUT);     //A1
-  pinMode(Pin2, OUTPUT);     //A2
+  pinMode(Pin1, OUTPUT);  //A1
+  pinMode(Pin2, OUTPUT);  //A2
  
-  pinMode(Pin3, OUTPUT);    //B1
-  pinMode(Pin4, OUTPUT);    //B2
+  pinMode(Pin3, OUTPUT);  //B1
+  pinMode(Pin4, OUTPUT);  //B2
  
-  Serial.begin(9600);          //Baud rate
+  Serial.begin(9600);    //Baud rate
 }
 
 
 void OneStep(int numstep)   // 8 Steps in one loop (0-7)
 {
-    switch(numstep)
+  switch(numstep)
   {
     case 0:
       digitalWrite(Pin1, HIGH);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
     case 1:
       digitalWrite(Pin1, HIGH);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, HIGH);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
     case 2:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, HIGH);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
     case 3:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, HIGH);
       digitalWrite(Pin3, HIGH);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
     case 4:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, HIGH);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
     case 5:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, HIGH);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, HIGH);
-    break;
-      case 6:
+      break;
+    case 6:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, HIGH);
-    break;
+      break;
     case 7:
       digitalWrite(Pin1, HIGH);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, HIGH);
-    break;
+      break;
     default:
       digitalWrite(Pin1, LOW);
       digitalWrite(Pin2, LOW);
       digitalWrite(Pin3, LOW);
       digitalWrite(Pin4, LOW);
-    break;
+      break;
   }
-
 }
 
  
 void loop()
 {
-  
   if(stepN < stepmax)
   {
-  OneStep(StepCount);
-  delay(20);
-  if(dir)
-  {
-    StepCount++;    // Move to the next step
-    stepN++;
-  }
-  else{
-    StepCount--;
-    stepN++;
-  }
-  if(StepCount>7){  // Reset the step, start from 0
-    StepCount=0;
-  }
-  if(StepCount<0){
-    StepCount=7;
-  }
+    OneStep(StepCount);
+    delay(20);
+    if(dir)
+    {
+      StepCount++;    // Move to the next step
+      stepN++;
+    }
+    else{
+      StepCount--;
+      stepN++;
+    }
+    if(StepCount>7){  // Reset the step, start from 0
+      StepCount=0;
+    }
+    if(StepCount<0){
+      StepCount=7;
+    }
   }
   else      
   {
@@ -117,7 +115,6 @@ void loop()
   }
   
   Serial.println(stepN);
-
 }
 
 
